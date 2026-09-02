b68c99bda094d4fda78f864a0d0bfdd74684eace
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-09-02 13:47:14 +0700

    test

--- patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/DisableSentryTelemetryPatch.kt
+++ patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/DisableSentryTelemetryPatch.kt
@@ -1,7 +1,6 @@
 package dev.jkcarino.revanced.patches.all.apkcleanup
 
 import app.revanced.patcher.patch.resourcePatch
-import app.shared.*
 import org.w3c.dom.Element
 
 /*
@@ -28,4 +27,56 @@ val disableSentryTelemetryPatch = resourcePatch(
             println("Disable Sentry telemetry: disabled $disabledComponents manifest components.")
         }
     }
+}
+
+// --- Helper extensions gom chung vào 1 file ---
+
+private fun Element.childrenNamed(name: String): List<Element> {
+    val nodes = childNodes
+    return buildList {
+        for (i in 0 until nodes.length) {
+            val node = nodes.item(i)
+            if (node is Element && node.nodeName == name) add(node)
+        }
+    }
+}
+
+private fun Element.childrenNamed(vararg names: String): List<Element> {
+    val acceptedNames = names.toSet()
+    val nodes = childNodes
+    return buildList {
+        for (i in 0 until nodes.length) {
+            val node = nodes.item(i)
+            if (node is Element && node.nodeName in acceptedNames) add(node)
+        }
+    }
+}
+
+private fun Element.getOrCreateApplicationMetaData(name: String): Element {
+    childrenNamed("meta-data")
+        .firstOrNull { it.getAttribute("android:name") == name }
+        ?.let { return it }
+
+    val metaData = ownerDocument.createElement("meta-data")
+    metaData.setAttribute("android:name", name)
+    appendChild(metaData)
+    return metaData
+}
+
+private fun Element.setApplicationMetaData(name: String, value: String) {
+    getOrCreateApplicationMetaData(name).setAttribute("android:value", value)
+}
+
+private fun Element.disableComponentsWhere(predicate: (String) -> Boolean): Int {
+    var disabled = 0
+
+    childrenNamed("activity", "provider", "service", "receiver")
+        .filter { component -> predicate(component.getAttribute("android:name")) }
+        .forEach { component ->
+            component.setAttribute("android:enabled", "false")
+            component.setAttribute("android:exported", "false")
+            disabled++
+        }
+
+    return disabled
 }
