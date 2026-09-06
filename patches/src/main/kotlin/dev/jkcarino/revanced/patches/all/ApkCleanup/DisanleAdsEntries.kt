54c318b5e49e5a931964cff4b60dd7392c8f85cf
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-09-07 00:30:14 +0700

    test

--- patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/DisanleAdsEntries.kt
+++ patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/DisanleAdsEntries.kt
@@ -1,93 +1,87 @@
-package dev.jkcarino.revanced.patches.all.apkcleanup
+package dev.revanced.patches.ads // Đổi tên package theo project ReVanced của bạn
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.extensions.InstructionExtensions.instructionsOrNull
-import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.data.AndroidManifestContext
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.patch.androidManifestPatch
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patcher.patch.resourcePatch
-import app.revanced.patcher.patch.stringOption
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction35c
-import com.android.tools.smali.dexlib2.iface.reference.FieldReference
-import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction10x
+import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction11n
+import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction11x
 import org.w3c.dom.Element
-import java.io.File
 
-@Suppress("unused")
-val universalRemoveAdManifestEntriesPatch = resourcePatch(
+object RemoveAdManifestEntriesPatch = androidManifestPatch(
     name = "Remove ad manifest entries",
     description = "Removes common ad SDK permissions, services, providers, libraries, and metadata.",
-    default = false,
+    use = false
 ) {
-    execute {
-        document("AndroidManifest.xml").use { doc ->
-            val adPermissions = setOf(
-                "com.google.android.gms.permission.AD_ID",
-                "android.permission.ACCESS_ADSERVICES_ATTRIBUTION",
-                "android.permission.ACCESS_ADSERVICES_AD_ID",
-                "android.permission.ACCESS_ADSERVICES_TOPICS",
-                "android.permission.ACCESS_ADSERVICES_CUSTOM_AUDIENCE",
-                "android.permission.AD_SERVICES_CONFIG",
-                "android.permission.KILL_BACKGROUND_PROCESSES",
-            )
-            val adNames = listOf(
-                "com.google.android.gms.ads",
-                "com.google.android.gms.measurement",
-                "com.facebook.ads",
-                "com.facebook.adspayments",
-                "com.facebook.adscomposer",
-                "com.facebook.adsconsentvalue",
-                "com.facebook.bugreporter",
-                "com.facebook.adinterfaces",
-                "com.facebook.adspreviewinjection",
-                "com.facebook.adpreview",
-                "com.facebook.adsexperiencetool",
-                "com.facebook.messaging.business.ads",
-                "com.facebook.messaging.nativepagereply.adcreation",
-                "com.facebook.messaging.professionalmode.adscreation",
-                "com.applovin",
-                "com.mbridge.msdk",
-                "com.inmobi.ads",
-                "com.unity3d.ads",
-                "com.vungle",
-                "com.ironsource",
-                "com.bytedance.sdk",
-                "com.anythink",
-                "com.qq.e.",
-                "com.baidu.mobads",
-                "com.kwad.sdk",
-                "com.sigmob",
-                "com.tradplus",
-                "com.pangle",
-            )
-            for (tag in listOf("uses-permission", "uses-library", "property", "meta-data", "provider", "service", "receiver", "activity")) {
-                val nodes = doc.getElementsByTagName(tag)
-                for (i in nodes.length - 1 downTo 0) {
-                    val node = nodes.item(i) as? Element ?: continue
-                    val name = node.getAttribute("android:name")
-                    val value = node.getAttribute("android:value")
-                    val remove = when (tag) {
-                        "uses-permission" -> name in adPermissions
-                        "uses-library" -> name == "android.ext.adservices"
-                        "property" -> name == "android.adservices.AD_SERVICES_CONFIG"
-                        else -> adNames.any { name.startsWith(it) || value.startsWith(it) }
-                    }
-                    if (remove) node.parentNode?.removeChild(node)
+    override fun execute(context: AndroidManifestContext) {
+        val doc = context.document
+        val adPermissions = setOf(
+            "com.google.android.gms.permission.AD_ID",
+            "android.permission.ACCESS_ADSERVICES_ATTRIBUTION",
+            "android.permission.ACCESS_ADSERVICES_AD_ID",
+            "android.permission.ACCESS_ADSERVICES_TOPICS",
+            "android.permission.ACCESS_ADSERVICES_CUSTOM_AUDIENCE",
+            "android.permission.AD_SERVICES_CONFIG",
+        )
+        val adNames = listOf(
+            "com.google.android.gms.ads",
+            "com.google.android.gms.measurement",
+            "com.facebook.ads",
+            "com.facebook.adspayments",
+            "com.facebook.adscomposer",
+            "com.facebook.adsconsentvalue",
+            "com.facebook.bugreporter",
+            "com.facebook.adinterfaces",
+            "com.facebook.adspreviewinjection",
+            "com.facebook.adpreview",
+            "com.facebook.adsexperiencetool",
+            "com.facebook.messaging.business.ads",
+            "com.facebook.messaging.nativepagereply.adcreation",
+            "com.facebook.messaging.professionalmode.adscreation",
+            "com.applovin",
+            "com.mbridge.msdk",
+            "com.inmobi.ads",
+            "com.unity3d.ads",
+            "com.vungle",
+            "com.ironsource",
+            "com.bytedance.sdk",
+            "com.anythink",
+            "com.qq.e.",
+            "com.baidu.mobads",
+            "com.kwad.sdk",
+            "com.sigmob",
+            "com.tradplus",
+            "com.pangle",
+        )
+
+        for (tag in listOf("uses-permission", "uses-library", "property", "meta-data", "provider", "service", "receiver", "activity")) {
+            val nodes = doc.getElementsByTagName(tag)
+            for (i in nodes.length - 1 downTo 0) {
+                val node = nodes.item(i) as? Element ?: continue
+                val name = node.getAttribute("android:name")
+                val value = node.getAttribute("android:value")
+                val remove = when (tag) {
+                    "uses-permission" -> name in adPermissions
+                    "uses-library" -> name == "android.ext.adservices"
+                    "property" -> name == "android.adservices.AD_SERVICES_CONFIG"
+                    else -> adNames.any { name.startsWith(it) || value.startsWith(it) }
+                }
+                if (remove) {
+                    node.parentNode?.removeChild(node)
                 }
             }
         }
     }
 }
 
-@Suppress("unused")
-val universalDisableAdSdkCallsPatch = bytecodePatch(
+object DisableAdSdkCallsPatch = bytecodePatch(
     name = "Disable ad SDK calls",
     description = "No-ops common ad SDK load/show/init/fetch methods in bundled ad packages.",
-    use = false,
+    use = false
 ) {
-    execute {
+    override fun execute(context: BytecodeContext) {
         val adPackages = listOf(
             "Lcom/applovin/",
             "Lcom/facebook/ads/",
@@ -110,58 +104,47 @@ val universalDisableAdSdkCallsPatch = bytecodePatch(
             "Lcom/pangle/",
         )
         val voidMethodNames = setOf(
-            "loadAd",
-            "loadAds",
-            "load",
-            "show",
-            "showAd",
-            "fetchAd",
-            "init",
-            "start",
-            "initSDK",
-            "initialize",
-            "initializeSdk",
-            "loadSplashAd",
-            "loadRewardVideoAd",
-            "loadInterstitialAd",
-            "loadBannerAd",
-            "loadNativeAd",
-            "loadFeedAd",
-            "loadNativeExpressAd",
-            "loadBannerExpressAd",
-            "loadDrawFeedAd",
-            "loadExpressDrawFeedAd",
-            "loadSplashScreenAd",
-            "showSplashView",
-            "showSplashClickEyeView",
-            "showSplashCardView",
-            "showRewardVideoAd",
-            "showFullScreenVideoAd",
-            "showInterstitialAd",
-            "showSplashMiniWindow",
-            "showSplashMiniWindowIfNeeded",
-            "showNativeAd",
-            "negativeFeedback",
-            "startLoadAd",
+            "loadAd", "loadAds", "load", "show", "showAd", "fetchAd", "init", "start",
+            "initSDK", "initialize", "initializeSdk", "loadSplashAd", "loadRewardVideoAd",
+            "loadInterstitialAd", "loadBannerAd", "loadNativeAd", "loadFeedAd",
+            "loadNativeExpressAd", "loadBannerExpressAd", "loadDrawFeedAd",
+            "loadExpressDrawFeedAd", "loadSplashScreenAd", "showSplashView",
+            "showSplashClickEyeView", "showSplashCardView", "showRewardVideoAd",
+            "showFullScreenVideoAd", "showInterstitialAd", "showSplashMiniWindow",
+            "showSplashMiniWindowIfNeeded", "showNativeAd", "negativeFeedback", "startLoadAd"
         )
         val objectMethodNames = setOf(
-            "getSplashView",
-            "getSplashClickEyeView",
-            "getSplashCardView",
-            "getBannerView",
-            "getFeedView",
+            "getSplashView", "getSplashClickEyeView", "getSplashCardView",
+            "getBannerView", "getFeedView"
         )
-        classDefForEach { classDef ->
-            if (adPackages.none { classDef.type.startsWith(it) }) return@classDefForEach
-            mutableClassDefBy(classDef).methods.forEach { method ->
-                if (method.name in voidMethodNames && method.returnType == "V" && method.name !in setOf("<init>", "<clinit>")) {
-                    method.addInstructions(0, "return-void")
+
+        context.classes.forEach { classDef ->
+            if (adPackages.none { classDef.type.startsWith(it) }) return@forEach
+
+            classDef.methods.forEach methodLoop@{ method ->
+                if (method.name in setOf("<init>", "<clinit>")) return@methodLoop
+
+                val implementation = method.implementation ?: return@methodLoop
+
+                val isVoid = method.name in voidMethodNames && method.returnType == "V"
+                val isObject = method.name in objectMethodNames && method.returnType.startsWith("L")
+                val isBool = method.name in setOf("isInitSuccess", "isSdkReady") && method.returnType == "Z"
+
+                if (!isVoid && !isObject && !isBool) return@methodLoop
+
+                // Đảm bảo method có ít nhất 1 register để return v0 không bị văng Exception
+                if (!isVoid && implementation.registerCount < 1) {
+                    implementation.registerCount = 1
                 }
-                if (method.name in objectMethodNames && method.returnType.startsWith("L")) {
-                    method.addInstructions(0, "const/4 v0, 0x0\nreturn-object v0")
-                }
-                if (method.name in setOf("isInitSuccess", "isSdkReady") && method.returnType == "Z") {
-                    method.addInstructions(0, "const/4 v0, 0x0\nreturn v0")
+
+                if (isVoid) {
+                    implementation.instructions.add(0, BuilderInstruction10x(Opcode.RETURN_VOID))
+                } else if (isObject) {
+                    implementation.instructions.add(0, BuilderInstruction11n(Opcode.CONST_4, 0, 0))
+                    implementation.instructions.add(1, BuilderInstruction11x(Opcode.RETURN_OBJECT, 0))
+                } else if (isBool) {
+                    implementation.instructions.add(0, BuilderInstruction11n(Opcode.CONST_4, 0, 0))
+                    implementation.instructions.add(1, BuilderInstruction11x(Opcode.RETURN, 0))
                 }
             }
         }
