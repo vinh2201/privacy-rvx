06bb4b3a9a54a46e5f8fcf5477a9eefdca22bb83
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-09-11 10:53:04 +0700

    test

--- patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/PngOptimizerPatch.kt
+++ patches/src/main/kotlin/dev/jkcarino/revanced/patches/all/ApkCleanup/PngOptimizerPatch.kt
@@ -10,8 +10,6 @@ import java.util.zip.Deflater
 import java.util.zip.Inflater
 import java.io.File
 
-private val logger = Logger.getLogger("PngOptimizerPatch")
-
 private val PNG_SIGNATURE = byteArrayOf(
     0x89.toByte(), 'P'.code.toByte(), 'N'.code.toByte(), 'G'.code.toByte(),
     0x0D, 0x0A, 0x1A, 0x0A,
@@ -156,6 +154,7 @@ val pngOptimizerPatch = resourcePatch(
     use = false,
 ) {
     execute {
+        val logger = Logger.getLogger(this::class.java.name)
         // Dùng trick getApkRoot để tìm đúng thư mục
         val manifestFile = get("AndroidManifest.xml")
         val apkRoot = getApkRoot(manifestFile)
